213f88ca129972a817dd6eae6aaf1498d4213396
Arkademy <ascalon0823@gmail.com>
2025-06-01 19:32:49 +0800

    set allow use any origin get from header

--- middleware/cors.go
+++ middleware/cors.go
@@ -8,7 +8,7 @@ import (
 
 func CorsMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Header("Access-Control-Allow-Origin", "http://127.0.0.1:5173")
+		c.Header("Access-Control-Allow-Origin", c.Request.Header.Get("Origin"))
 		c.Header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		c.Header("Access-Control-Allow-Headers", "Origin, Content-Type, Authorization")
 		c.Header("Access-Control-Allow-Credentials", "true")
